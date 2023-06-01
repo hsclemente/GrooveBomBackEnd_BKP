69975ed514a6af959fc558e6a1a1fbb799d941d3
borbinhaa <gabrieldeborba07@gmail.com>
2023-05-31 21:16:15 -0300

    Atualizando

--- src/main/java/br/com/hc/groove/bom/domain/models/entities/Tarefa.java
+++ src/main/java/br/com/hc/groove/bom/domain/models/entities/Tarefa.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import br.com.hc.groove.bom.domain.models.forms.TarefaForm;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @Table(name = "gb_tarefas")
 @AllArgsConstructor@NoArgsConstructor
